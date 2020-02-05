3e3a702c65f4d2cb35b162a3ba855da9706eb412
geoMADE <geomad@leeds.ac.uk>
2020-02-05 12:01:46 +0000

    asdfdsafdsf

--- script.R
+++ script.R
@@ -9,7 +9,7 @@
 
 #setwd("C:/Users/monsu/Desktop/software paper/")
 setwd("C:/Users/monsu/Desktop/software paper/")
-setwd("C:/R/tweets/")
+#setwd("C:/R/tweets/")
 
 getwd()
 
@@ -126,8 +126,8 @@ for(i in 1:length(unique_country)){ #i<-1
 uniq_Dates <- unique(all_Tweets$created_at)
 uniq_Dates <- uniq_Dates[order(uniq_Dates)]
 
-write_as_csv(all_Tweets, "C:/Users/monsu/Documents/GitHub/Sentiment-analysis/scottishIndy_byCountry_TRIAL28.csv", na="NA", fileEncoding = "UTF-8")
-write.table(uniq_Dates, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis/scottishIndy_byCountry_uniq_Dates_TRIAL28.csv", sep=",", row.names = F)
+write_as_csv(all_Tweets, "C:/Users/monsu/Documents/GitHub/Sentiment-analysis/scottishIndy_byCountry_TRIAL37.csv", na="NA", fileEncoding = "UTF-8")
+write.table(uniq_Dates, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis/scottishIndy_byCountry_uniq_Dates_TRIAL37.csv", sep=",", row.names = F)
 
 
 #write_as_csv(all_Tweets, file="C:/R/tweets/scottishIndy_byCountry_TRIAL18.csv", na="NA", fileEncoding = "UTF-8")
@@ -229,8 +229,8 @@ for(j in 1:nrow(p_area_)){  #j<-1
 uniq_Dates <- unique(all_Tweets$created_at)
 uniq_Dates <- uniq_Dates[order(uniq_Dates)]
 
-write_as_csv(all_Tweets, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis/download_byVoting_TRIAL28.csv", na="NA", fileEncoding = "UTF-8")
-write.table(uniq_Dates, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis/download_byVoting_uniq_Dates_TRIAL28.csv", sep=",", row.names = F)
+write_as_csv(all_Tweets, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis/download_byVoting_TRIAL37.csv", na="NA", fileEncoding = "UTF-8")
+write.table(uniq_Dates, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis/download_byVoting_uniq_Dates_TRIAL37.csv", sep=",", row.names = F)
 #w
 
 #write_as_csv(all_Tweets, file="C:/R/tweets/download_byVoting_TRIAL15.csv", na="NA", fileEncoding = "UTF-8")
@@ -312,8 +312,8 @@ for(i in 1:length(unique_country)){ #i<-1
 uniq_Dates <- unique(all_Tweets$created_at)
 uniq_Dates <- uniq_Dates[order(uniq_Dates)]
 
-write_as_csv(all_Tweets, "C:/Users/monsu/Documents/GitHub/Sentiment-analysis/download_byCountry_TRIAL28.csv", na="NA", fileEncoding = "UTF-8")
-write.table(uniq_Dates, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis/download_byCountry_uniq_Dates_TRIAL28.csv", sep=",", row.names = F)
+write_as_csv(all_Tweets, "C:/Users/monsu/Documents/GitHub/Sentiment-analysis/download_byCountry_TRIAL37.csv", na="NA", fileEncoding = "UTF-8")
+write.table(uniq_Dates, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis/download_byCountry_uniq_Dates_TRIAL37.csv", sep=",", row.names = F)
 
 # 
 
@@ -329,7 +329,7 @@ write.table(uniq_Dates, file="C:/Users/monsu/Documents/GitHub/Sentiment-analysis
 
 
 
-
+0
 
 /'
 
